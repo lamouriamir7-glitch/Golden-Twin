eddc61587bd78ef8564c6f229d6e17a9f871da77
lamouriamir7-glitch <lamouriamir7@gmail.com>
2026-05-06 04:36:44 +0100

    Create App.tsx

---
+++ SRC/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import Index from "./pages/Index";
+import Auth from "./pages/Auth";
+import Fingerprint from "./pages/Fingerprint";
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-center" richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/fingerprint" element={<Fingerprint />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
